ad011e398c9049088f514f05db710e7b9e5fbf88
HaillaPereiraifba <haillapereira91@gmail.com>
2026-03-03 09:09:53 -0300

    Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,14 @@
+class TV{
+  int tamanho;
+  int volume = 5; 
+  String marca;
+  int voltage; 
+  int canal;
 
+  int aumentar volume (){
+      if (volume < 10){
+    volume ++;
+  }
+  return volume;
+  }
+}
