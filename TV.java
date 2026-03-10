14ad63f24221bb822a4701794703aaba6b916ce3
HaillaPereiraifba <haillapereira91@gmail.com>
2026-03-09 21:31:58 -0300

    Refactor TV class and add main method for testing

--- TV.java
+++ TV.java
@@ -1,49 +1,60 @@
 public class TV {
 
-    //Características da TV
-    int tamanhoTela; // tamanho da tela em polegadas
-    int volume = 5;  // volume começa em 5 (mínimo 1 e máximo 10)
-    String marca; // marca da TV
-    int voltagem; // 110 ou 220
-    int canal = 1; // canal inicial
+    int tamanhoTela;
+    int volume = 5;
+    String marca;
+    int voltagem;
+    int canal = 1;
 
-    // Método para ligar a TV
     void ligar() {
-        int consumo = voltagem * tamanhoTela;// calcula o consumo
+        int consumo = voltagem * tamanhoTela;
         System.out.println("TV ligada");
         System.out.println("Consumo: " + consumo);
     }
 
-    // Método para desligar a TV
     void desligar() {
         System.out.println("TV desligada");
     }
 
-    // Aumenta o volume
     void aumentarVolume() {
-        if (volume < 10) { //só aumenta se for menor que 10
+        if (volume < 10) {
             volume++;
         }
         System.out.println("Volume: " + volume);
     }
 
-    // Diminui o volume
     void diminuirVolume() {
-        if (volume>1) { // só diminui se for maior que 1
+        if (volume > 1) {
             volume--;
         }
-        System.out.println("Volume: "+volume);
+        System.out.println("Volume: " + volume);
     }
 
-    // Passa para o próximo canal
     void subirCanal() {
         canal++;
-        System.out.println("Canal: "+canal);
+        System.out.println("Canal: " + canal);
     }
 
-    // Volta para o canal anterior
     void descerCanal() {
-        canal--;
-        System.out.println("Canal: "+canal);
+        if (canal > 1) {
+            canal--;
+        }
+        System.out.println("Canal: " + canal);
     }
-}
+
+    public static void main(String[] args) {
+
+        TV tv = new TV();
+
+        tv.tamanhoTela = 50;
+        tv.voltagem = 220;
+        tv.marca = "LG";
+
+        tv.ligar();
+        tv.aumentarVolume();
+        tv.diminuirVolume();
+        tv.subirCanal();
+        tv.descerCanal();
+        tv.desligar();
+    }
+} 
